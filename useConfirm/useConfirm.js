dfba3c08507bb80057211829c83920a8fc179ff8
webdevyoo <yjk7454@naver.com>
2024-01-20 00:11:36 +0900

    커스텀 훅 useConfirm 추가 및 내보내기

---
+++ useConfirm/useConfirm.js
@@ -0,0 +1,11 @@
+const useConfirm = (message = "", onConfirm, onCancel) => {
+  if (typeof onConfirm !== "function" && typeof onCancel !== "function") return;
+  const confirmAction = () => {
+    if (confirm(message)) {
+      onConfirm("Success");
+    } else {
+      onCancel("Failed");
+    }
+  };
+  return confirmAction;
+};
